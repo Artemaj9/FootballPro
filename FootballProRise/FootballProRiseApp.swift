3694928a21a501e592b1ccf68b646a748f459d30
ArtemMoiseev <moiseev@physics.msu.ru>
2024-02-22 01:55:22 +0300

    Initial Commit

---
+++ FootballProRise/FootballProRiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballProRiseApp.swift
+//  FootballProRise
+//
+//  Created by Artem on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FootballProRiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
